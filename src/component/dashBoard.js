3f32a0a9fa5ad02e9d90c86f2096024582ac2978
Moatsem <m3tsmsh@gmail.com>
2022-10-10 20:15:02 +0200

    applying filters to products

--- src/component/dashBoard.js
+++ src/component/dashBoard.js
@@ -4,56 +4,64 @@ import ProductThumbnail from "./productThumbnail";
 import { handleProducts } from "../actions/shared";
 import { getAttributes } from "./filter";
 class DashBoard extends Component {
- 
+  state = {
+    filteredProducts: null,
+  };
 
   componentDidMount() {
     if (this.props.loading) {
       this.props.dispatch(handleProducts());
-      this.filterProdcts(this.props.products, "Size");
     } else return;
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.location.search !== this.props.location.search) {
-      //products = this.props;
+      console.log(this.props.location.search, "url from dashboard");
+      const params = new URLSearchParams(this.props.location.search);
+      const size = params.get("Size"); // bar
+      const capacity = params.get("Capacity"); // bar
+
+      console.log(params, " search params from dash board");
+      console.log(size, " size from dash board");
+      console.log(capacity, " size from dash board");
+
+      const filteredProducts = this.filterProducts(this.props.products, "Size");
+      this.setState({
+        filteredProducts
+      });
+      console.log(filteredProducts, " 111from filtr rsult fnction ");
       console.log("products list ubdated ");
     }
   }
 
+  filterProducts = (products, filter) => {
+    const filteredProducts = [];
+
+    for (let product of products) {
+      let attributes = product.attributes?.filter(
+        (atr) => atr?.name === filter
+      );
+      if (attributes.length > 0) filteredProducts.push(product);
+    }
+   
+    return filteredProducts;
+  };
+
+  //apply filters on products
+
   render() {
-    console.log(this.props.location.search, "url from dashboard");
-    const params = new URLSearchParams(this.props.location.search);
-    const size = params.get("Size"); // bar
-    const capacity = params.get("Capacity"); // bar
-
-    console.log(params, " search params from dash board");
-    console.log(size, " size from dash board");
-    console.log(capacity, " size from dash board");
-
     const { category, loading } = this.props;
-    const { products } = this.props;
+    const  products  = this.state.filteredProducts||this.props.products;
 
     //let atrs=getAttributes(products)
-
+console.log(this.state.filteredProducts,"state form dash board");
     console.log(products, "test filter on those");
-   // console.log(atrs, "test atrs  on those");
+    // console.log(atrs, "test atrs  on those");
 
     //   console.log("this category dashboard page "+category)
+    //const filteredProducts=this.filterProdcts(this.props.products, "Size")
 
-    const filterProdcts = (products, filter) => {
-      const filteredProducts=[]
-
-       for(let product of products) {
-        let attributes = product.attributes?.filter(
-          (atr) => atr?.name === filter
-        );
-        if (attributes.length > 0) filteredProducts.push(product);
-
-       }
-
-       return filteredProducts
-    };
-    console.log( filterProdcts(products,"Size"), "from filtr rsult fnction ");
+    // console.log( filteredProducts, " 222 from filtr rsult fnction ");
 
     return (
       <div>
